4e290b8cbbfffa6a0eeb8d74f837cd9e2f1010bc
Marcin van de Ven <marcinvandeven@gmail.com>
2020-05-05 18:25:23 +0200

    Moved data to general component
    Moved the data away such that the switcher uses @Prop-method to get data.

--- src/components/ProductSwitcher.vue
+++ src/components/ProductSwitcher.vue
@@ -15,7 +15,7 @@
 
 <script lang="ts">
 // @ is an alias to /src
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue';
 import ProductCategoryOverview from '@/components/ProductCategoryOverview.vue';
 import { Product } from '@/entities/Product';
@@ -27,118 +27,8 @@ import { ProductCategory } from '@/entities/ProductCategory';
   },
 })
 export default class ProductSwitcher extends Vue {
-  // *************************************************
-  //
-  //               Begin test data
-  //
-  // *************************************************
-  beugel: Product = {
-    id: '1',
-    name: 'Grolsch beugel',
-    ownerId: '1',
-    price: 110,
-    picture: 'https://deklokdranken.blob.core.windows.net/product-images/101007.jpg',
-    traySize: 20,
-    category: 'drink',
-    isAlcoholic: true,
-    negative: false,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  };
-
-  tripel: Product = {
-    id: '2',
-    name: 'Grimbergen tripel (voor de sfeer)',
-    ownerId: '1',
-    price: 90,
-    picture: 'https://deklokdranken.blob.core.windows.net/product-images/105120.jpg',
-    traySize: 24,
-    category: 'drink',
-    isAlcoholic: true,
-    negative: false,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  };
-
-  radler: Product = {
-    id: '3',
-    name: 'Radler',
-    ownerId: '1',
-    price: 50,
-    picture: 'https://deklokdranken.blob.core.windows.net/product-images/1021123.jpg',
-    traySize: 1,
-    category: 'drink',
-    isAlcoholic: true,
-    negative: false,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  };
-
-  cola: Product = {
-    id: '4',
-    name: 'Cola',
-    ownerId: '2',
-    price: 70,
-    picture: 'https://deklokdranken.blob.core.windows.net/product-images/201003.jpg',
-    traySize: 1,
-    category: 'drink',
-    isAlcoholic: false,
-    negative: false,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  };
-
-  cassis: Product = {
-    id: '5',
-    name: 'Cassis',
-    ownerId: '2',
-    price: 70,
-    picture: 'https://deklokdranken.blob.core.windows.net/product-images/205053.jpg',
-    traySize: 1,
-    category: 'drink',
-    isAlcoholic: false,
-    negative: false,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  };
-
-  fristi: Product = {
-    id: '6',
-    name: 'Fristi',
-    ownerId: '2',
-    price: 90,
-    picture: 'https://deklokdranken.blob.core.windows.net/product-images/242052.jpg',
-    traySize: 1,
-    category: 'drink',
-    isAlcoholic: false,
-    negative: false,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  };
-
-  beers: Product[] = [this.beugel, this.tripel, this.radler];
-
-  sodas: Product[] = [this.cola, this.cassis, this.fristi];
-
-  beerProducts: ProductCategory = {
-    id: '1',
-    name: 'zuiphulp',
-    products: this.beers,
-  };
-
-  sodaProducts: ProductCategory = {
-    id: '2',
-    name: 'laf',
-    products: this.sodas,
-  };
-
-  productCategories: ProductCategory[] = [this.beerProducts, this.sodaProducts];
-
-  // *************************************************
-  //
-  //               End test data
-  //
-  // *************************************************
+  @Prop()
+  productCategories!: ProductCategory[];
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <ProductSwitcher />
+    <ProductSwitcher :productCategories="productCategories"/>
   </div>
 </template>
 
@@ -9,6 +9,7 @@
 import { Component, Vue } from 'vue-property-decorator';
 import { Product } from '@/entities/Product';
 import ProductSwitcher from '@/components/ProductSwitcher.vue';
+import { ProductCategory } from '@/entities/ProductCategory';
 
 @Component({
   components: {
@@ -16,6 +17,117 @@ import ProductSwitcher from '@/components/ProductSwitcher.vue';
   },
 })
 export default class Home extends Vue {
+// *************************************************
+  //
+  //               Begin test data
+  //
+  // *************************************************
+  beugel: Product = {
+    id: '1',
+    name: 'Grolsch beugel',
+    ownerId: '1',
+    price: 110,
+    picture: 'https://deklokdranken.blob.core.windows.net/product-images/101007.jpg',
+    traySize: 20,
+    category: 'drink',
+    isAlcoholic: true,
+    negative: false,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
 
+  tripel: Product = {
+    id: '2',
+    name: 'Grimbergen tripel (voor de sfeer)',
+    ownerId: '1',
+    price: 90,
+    picture: 'https://deklokdranken.blob.core.windows.net/product-images/105120.jpg',
+    traySize: 24,
+    category: 'drink',
+    isAlcoholic: true,
+    negative: false,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
+  radler: Product = {
+    id: '3',
+    name: 'Radler',
+    ownerId: '1',
+    price: 50,
+    picture: 'https://deklokdranken.blob.core.windows.net/product-images/1021123.jpg',
+    traySize: 1,
+    category: 'drink',
+    isAlcoholic: true,
+    negative: false,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
+  cola: Product = {
+    id: '4',
+    name: 'Cola',
+    ownerId: '2',
+    price: 70,
+    picture: 'https://deklokdranken.blob.core.windows.net/product-images/201003.jpg',
+    traySize: 1,
+    category: 'drink',
+    isAlcoholic: false,
+    negative: false,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
+  cassis: Product = {
+    id: '5',
+    name: 'Cassis',
+    ownerId: '2',
+    price: 70,
+    picture: 'https://deklokdranken.blob.core.windows.net/product-images/205053.jpg',
+    traySize: 1,
+    category: 'drink',
+    isAlcoholic: false,
+    negative: false,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
+  fristi: Product = {
+    id: '6',
+    name: 'Fristi',
+    ownerId: '2',
+    price: 90,
+    picture: 'https://deklokdranken.blob.core.windows.net/product-images/242052.jpg',
+    traySize: 1,
+    category: 'drink',
+    isAlcoholic: false,
+    negative: false,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
+  beers: Product[] = [this.beugel, this.tripel, this.radler];
+
+  sodas: Product[] = [this.cola, this.cassis, this.fristi];
+
+  beerProducts: ProductCategory = {
+    id: '1',
+    name: 'zuiphulp',
+    products: this.beers,
+  };
+
+  sodaProducts: ProductCategory = {
+    id: '2',
+    name: 'laf',
+    products: this.sodas,
+  };
+
+  productCategories: ProductCategory[] = [this.beerProducts, this.sodaProducts];
+
+  // *************************************************
+  //
+  //               End test data
+  //
+  // *************************************************
 }
 </script>
