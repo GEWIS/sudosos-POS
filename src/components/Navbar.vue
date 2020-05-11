3168fc909be74711926e91650bac979094e27daf
Roy (Laptop) <r.t.b.kakkenberg@student.tue.nl>
2020-05-11 19:50:11 +0200

    Finished Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,13 @@
 <template>
-  <b-nav :vertical="vertical" class="nav mt-md-0 mt-3">
-    <b-nav-item><img src="@/assets/img/gewis-branding.svg" alt="GEWIS Logo"/></b-nav-item>
+  <b-nav :vertical="vertical" class="nav"
+         v-bind:class="{ vertical: vertical, horizontal:!vertical }">
+    <b-nav-item class="gewis-logo"><img src="@/assets/img/gewis-branding.svg" alt="GEWIS Logo"/>
+    </b-nav-item>
     <b-nav-item><font-awesome-icon icon="beer" /></b-nav-item>
     <b-nav-item><font-awesome-icon icon="coffee" /></b-nav-item>
     <b-nav-item><font-awesome-icon icon="cookie-bite" /></b-nav-item>
-    <b-nav-item><font-awesome-icon icon="ellipsis-h" /></b-nav-item>
+    <b-nav-item><font-awesome-icon icon="ticket-alt" /></b-nav-item>
+    <b-nav-item class="other-button"><font-awesome-icon icon="ellipsis-h" /></b-nav-item>
   </b-nav>
 </template>
 
@@ -14,10 +17,56 @@ import { Component, Vue } from 'vue-property-decorator';
 import '@/styles/Nav.scss';
 
 export default class App extends Vue {
-    public vertical: boolean = window.innerWidth > 600;
+    public vertical: boolean = window.innerWidth > 768;
 }
 </script>
 
 <style scoped>
+.vertical {
+  height: 100vh;
+  width: 6rem;
+}
 
+.vertical > li {
+  width: 100%
+}
+
+.vertical > li > a {
+  width: 100%
+}
+
+.vertical > li.other-button {
+  margin-top: auto;
+}
+
+.horizontal {
+  width: 100vw;
+  height: 4rem;
+  font-size: 12px;
+  line-height: 1.2;
+}
+
+.horizontal > li {
+  height: 100%;
+  width: calc(100% / 6)
+}
+
+.horizontal > li > a {
+  height: 100%;
+  margin: auto 0;
+  padding: 0;
+}
+
+.horizontal > li > a > svg {
+  font-size: 30px;
+}
+
+.horizontal > li.gewis-logo > a {
+  padding: 0.4rem 0.2rem !important;
+}
+
+.horizontal > li.gewis-logo > a > img {
+  margin-top: -0.4rem !important;
+  height: 100%;
+}
 </style>
