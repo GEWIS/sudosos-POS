40d24e4d8ff2b9a8e7ac639fdd182fb1d05b85e1
Marcin van de Ven <marcinvandeven@gmail.com>
2020-05-12 17:14:53 +0200

    Removed use of 'home' component for better testing

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+<!--    <div id="nav">-->
+<!--      <router-link to="/">Home</router-link> |-->
+<!--      <router-link to="/about">About</router-link>-->
+<!--    </div>-->
+    <Checkout />
+    <ProductListComponent hidden></ProductListComponent>
+<!--    <router-view/>-->
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import ProductListComponent from '@/components/ProductListComponent.vue';
+import Checkout from '@/components/Checkout.vue';
+
+  @Component({
+    components: { Checkout, ProductListComponent },
+  })
+
+export default class App extends Vue {
+}
+</script>
 
 <style lang="scss">
 #app {
