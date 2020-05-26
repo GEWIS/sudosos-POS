6b026db92aa08501200f9443c1e2d75e8d18b79b
Ruben Brinkman <Rubenjulianbrinkman@gmail.com>
2020-05-26 17:29:23 +0200

    Fixed checkout-footer

--- src/components/Checkout.vue
+++ src/components/Checkout.vue
@@ -1,5 +1,6 @@
 <template>
-  <b-container fluid class="h-100">
+  <div class="h-100">
+    <div id="wrapper">
     <!-- Checkout header -->
     <b-row class="head" align-v="center">
       <b-col cols="10">
@@ -8,13 +9,9 @@
             <font-awesome-icon icon="user"></font-awesome-icon>
           </b-col>
           <b-col cols="9" class="text-left username">
-            <b-row cols="12">
-              <!-- TODO: is dit handig? -->
-              <!-- Zie: https://code.luasoftware.com/tutorials/bootstrap/bootstrap-hide-element-based-on-viewport-size/ -->
-              <span>Marcin</span>
-              <span class="d-none d-md-block ml-2">van de Ven</span>
-              <span class="d-block d-md-none ml-2">(1234)</span>
-            </b-row>
+              <span class="d-inline">Marcin</span>
+              <span class="d-none d-md-inline ml-2">van de Ven</span>
+              <span class="d-inline d-md-none ml-2">(1234)</span>
           </b-col>
         </b-row>
         <b-row align-h="start" align-v="center" class="pt-2">
@@ -22,7 +19,7 @@
             <font-awesome-icon icon="wallet" ></font-awesome-icon>
           </b-col>
           <b-col cols="9" class="text-left balance">
-            <b-row cols="12">{{ dinero({amount: 1337}).toFormat() }}</b-row>
+            {{ dinero({amount: 1337}).toFormat() }}
           </b-col>
         </b-row>
       </b-col>
@@ -32,21 +29,28 @@
     </b-row>
 
     <!-- Checkout list -->
-    <b-row class="pb-2" cols="12"><b-col>
+    <b-row class="pb-2">
+      <b-col cols="12">
       <CheckoutList></CheckoutList>
-    </b-col></b-row>
+    </b-col>
+    </b-row>
+
+      <div id="push"></div>
+    </div>
+
+    <div id="checkout-buttons">
 
     <!-- Checkout calculation -->
-    <b-row class="bottom-fix w-100" cols="12">
-    <b-row class="summary ml-2 mr-2 summary-border">
-        <b-col class="text-left ml-n2">Total</b-col>
-        <b-col class="text-right mr-n2 price">
+    <b-row class="summary mx-2 summary-border">
+        <b-col cols="6" class="text-left ml-n2">Total</b-col>
+        <b-col cols="6" class="text-right mr-n2 price">
           {{ dinero({amount: 337}).toFormat() }}
         </b-col>
       </b-row>
-      <b-row class="summary ml-2 mr-2 ">
-        <b-col class="text-left ml-n2">Balance after</b-col>
-        <b-col class="text-right mr-n2 balance">
+
+      <b-row class="summary mx-2">
+        <b-col cols="6" class="text-left ml-n2">Balance after</b-col>
+        <b-col cols="6" class="text-right mr-n2 balance">
           {{ dinero({amount: 1337}).toFormat() }}
         </b-col>
       </b-row>
@@ -54,7 +58,7 @@
     <!-- Checkout button -->
 
     <b-row align-v="center" class="bottom-button checkout-button">
-      <b-col class="text-center">
+      <b-col cols="12" class="text-center">
         <font-awesome-icon icon="check" ></font-awesome-icon> Checkout
       </b-col>
     </b-row>
@@ -68,12 +72,12 @@
         Charge someone
       </b-col>
     </b-row>
-    </b-row>
-  </b-container>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component } from 'vue-property-decorator';
 import CheckoutList from '@/components/CheckoutList.vue';
 import Formatters from '@/mixins/Formatters';
 
@@ -86,28 +90,40 @@ export default class Checkout extends Formatters {
 </script>
 
 <style scoped lang="scss">
-.head {
-  background: $gewis-black;
-  color: white;
-  font-size: x-large;
-  padding: 0.5rem;
-}
+  @import '~bootstrap/scss/bootstrap';
 
-.balance {
-  color: $gewis-green;
-}
+  #wrapper {
+    min-height: 100%;
+    margin-bottom: -234px;
+  }
 
-.price {
-  color: $gewis-red;
-}
+  #checkout-buttons,
+  #push {
+    height: 234px;
+  }
 
-.summary {
-  font-size: x-large;
-}
+  .head {
+    background: $gewis-black;
+    color: white;
+    font-size: x-large;
+    padding: 0.5rem;
+  }
 
-.summary-border {
-  border-top: 0.13rem solid black;
-}
+  .balance {
+    color: $gewis-green;
+  }
+
+  .price {
+    color: $gewis-red;
+  }
+
+  .summary {
+    font-size: x-large;
+  }
+
+  .summary-border {
+    border-top: 0.13rem solid black;
+  }
 
   .bottom-fix {
     position: fixed;
@@ -115,12 +131,12 @@ export default class Checkout extends Formatters {
     bottom: 0;
   }
 
-.checkout-options {
-  background: $gewis-black;
-  .option-border {
-    border-right: 0.13rem solid white;
+  .checkout-options {
+    background: $gewis-black;
+    .option-border {
+      border-right: 0.13rem solid white;
+    }
   }
-}
 
   .bottom-button { // Small
     color: white;
@@ -132,8 +148,7 @@ export default class Checkout extends Formatters {
     background: $gewis-green;
   }
 
-
-  @media(min-width: 768px){ // Tablets
+  @include media-breakpoint-up(md) { // Tablets
     .bottom-button {
       padding: 1rem;
       font-size: x-large;
@@ -145,15 +160,11 @@ export default class Checkout extends Formatters {
     }
   }
 
-  @media(min-width: 992px){ // Desktops
+  @include media-breakpoint-up(lg) { // Desktops
     .bottom-button {
-      padding: 1rem;
-      padding-left: 2rem;
-      padding-right: 2rem;
+      padding: 1rem 2rem;
       font-size: xx-large;
       font-weight: bolder;
     }
   }
-
-
 </style>
