8e9a91159013a4e2458d1963bd5b252b27ff3c32
Ruben Brinkman <Rubenjulianbrinkman@gmail.com>
2020-06-02 15:52:26 +0200

    Fixed navbar such that view now renders correctly

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,15 +4,16 @@
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
 import ProductListComponent from '@/components/ProductListComponent.vue';
 
-export default {
-  name: 'home',
+@Component({
   components: {
     ProductListComponent,
   },
-};
+})
+export default class Home extends Vue {
+
+}
 </script>
