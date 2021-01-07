088bd53ba64f99294238a8e0a7b4c22971c1c5c0
Rick Wouters <rick.wouters@wolfpackit.nl>
2021-01-07 14:29:50 +0100

    Searchbar with integrated keyboard

---
+++ src/components/SearchbarWithKeyboard.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="searchbar-container" v-show="showSearchbar">
+    <div class="query-container">
+      <p class="query-text">{{ syncedInput }}</p>
+    </div>
+    <div class="keyboard-container"></div>
+  </div>
+</template>
+<script lang="ts">
+import {
+  Component, Prop, PropSync, Vue,
+} from 'vue-property-decorator';
+import Keyboard from 'simple-keyboard';
+import 'simple-keyboard/build/css/index.css';
+@Component
+export default class SearchbarWithKeyboard extends Vue {
+  @PropSync('input', { type: String }) syncedInput!: string;
+
+  @Prop() showSearchbar!: boolean;
+
+  private keyboard: any = null;
+
+  mounted() {
+    this.keyboard = new Keyboard('.keyboard-container', {
+      onChange: this.onChange,
+    });
+  }
+
+  onChange(input: string) {
+    this.syncedInput = input.charAt(0).toUpperCase() + input.slice(1);
+  }
+}
+</script>
+<style lang="scss" scoped>
+  .searchbar-container {
+    position: absolute;
+    bottom: 0;
+    left: 6rem;
+    width: calc(100% - 6rem);
+
+    .query-container {
+      p.query-text {
+        font-size: 4em;
+        margin-left: auto;
+        margin-right: auto;
+        width: fit-content;
+      }
+    }
+  }
+</style>
