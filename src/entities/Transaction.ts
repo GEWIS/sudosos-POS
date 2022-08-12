a2ca950ab0ebba159b15bfb6a9344952042e3bd4
Roy Kakkenberg <me@roykakkenberg.nl>
2022-08-12 12:43:13 +0200

    Change transaction value to Dinero instance

--- src/entities/Transaction.ts
+++ src/entities/Transaction.ts
@@ -1,15 +1,15 @@
-import { Dinero, DineroObject } from 'dinero.js';
 import { BaseUser, User } from '@/entities/User';
 import { BaseEntity } from '@/entities/BaseEntity';
 import { BasePointOfSale, PointOfSale } from '@/entities/PointOfSale';
 import { SubTransaction } from '@/entities/SubTransaction';
+import { Dinero } from 'dinero.js';
 
 export interface Transaction extends BaseEntity {
   from: BaseUser | User;
   createdBy?: BaseUser | User;
   pointOfSale: BasePointOfSale | PointOfSale;
   subTransactions: SubTransaction[];
-  price: DineroObject;
+  price: Dinero;
 }
 
 export interface POSTransaction {
