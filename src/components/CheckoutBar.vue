df100452071b379246c5d08c6024ca10b790915b
Roy Kakkenberg <me@roykakkenberg.nl>
2022-09-30 15:07:29 +0200

    Add "balance after" field to everyone when POS does not require auth

--- src/components/CheckoutBar.vue
+++ src/components/CheckoutBar.vue
@@ -34,7 +34,7 @@
         <div class="total-text">Total</div>
         <div class="total-value">€{{ (transactionTotal / 100).toFixed(2) }}</div>
       </div>
-      <div class="balance-row" v-if="!searchState.isChargingUser && pointOfSaleState.pointOfSale.useAuthentication">
+      <div class="balance-row" v-if="pointOfSaleState.pointOfSale.useAuthentication ? !searchState.isChargingUser : searchState.isChargingUser">
         <div class="balance-text">Balance after</div>
         <div class="balance-value warn" v-if="balanceAfter.getAmount() < 0">
           {{ balanceAfter.toFormat() }}
