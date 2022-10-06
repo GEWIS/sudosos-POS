6bd50acf05f61202578b012a703d62ce71233539
Jort van Driel <10116429+JortvD@users.noreply.github.com>
2022-10-06 16:54:10 +0200

    Add exit button
    Still need to re-use this component

--- src/views/ProductOverview.vue
+++ src/views/ProductOverview.vue
@@ -20,11 +20,7 @@
         </div>
         <div v-if="state === State.SEARCH || state === State.USER_SEARCH"
           class="nav align-items-center">
-          <div class="nav-item active exit-search-button" @click="exitSearch()">
-            <div class="nav-link">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z"/></svg>
-            </div>
-          </div>
+          <exit-button @click="exitSearch()" />
           <div class="nav-item search-text" @click="focusOnSearch()">
             <font-awesome-icon icon="search"/>
             <span class="text">{{state === State.SEARCH ? query : userQuery}}</span>
@@ -167,6 +163,7 @@ import ProductComponent from '@/components/ProductComponent.vue';
 import CategorieButtons from '@/components/CategorieButtons.vue';
 import CheckoutBar from '@/components/CheckoutBar.vue';
 import SearchModule from '@/store/modules/search';
+import ExitButton from '@/components/ExitButton.vue';
 
 import { SubTransactionRow } from '@/entities/SubTransactionRow';
 import { User, UserType } from '@/entities/User';
@@ -196,6 +193,7 @@ enum State {
     CheckoutBar,
     SettingsComponent,
     Keyboard,
+    ExitButton,
   },
 })
 export default class ProductOverview extends Vue {
