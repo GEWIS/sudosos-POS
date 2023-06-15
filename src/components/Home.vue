f7830fe7bac2d648fcf021dec4fd739a2ee2fa4b
Jort van Driel <10116429+JortvD@users.noreply.github.com>
2023-06-15 16:58:27 +0200

    More hotfix

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -128,14 +128,6 @@ export default class Home extends Vue {
 
     this.$watch('timedOut', (value) => {
       return;
-      if (value && !this.checkingOut) {
-        if (!this.cartState.isEmpty) {
-          this.$refs.checkoutBar.$refs.checkoutButton.checkout();
-        } else {
-          this.logout();
-          this.activityTimerState.setTimedOut(false);
-        }
-      }
     });
 
     document.addEventListener('contextmenu', (event) => {
